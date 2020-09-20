d04ac50ac68b5a125dffd92b3cd622222ffcd3ad
Arnav Borborah <arnavborborah11@gmail.com>
2020-09-20 09:20:51 -0400

    Clean up code here and there

--- chrome-extension/src/js/popup.js
+++ chrome-extension/src/js/popup.js
@@ -1,17 +1,15 @@
 import "../css/popup.css";
-function setup() {
-  let params = {
-    active: true,
-    currentWindow: true,
-  };
-  chrome.tabs.query(params, gotTab);
-  var username = "Chair"; // FROM BACKEND WHEN LOGINING
-  function gotTab(tabs) {
-    console.log("Hello from Rithvik");
-    console.log(tabs);
-    var val = tabs[0].url;
-    addUrl.textContent = "Url: " + tabs[0].url;
-  }
+
+function getUrl() {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      addUrl.textContent = tabs[0].url;
+    }
+  );
 }
 
-setup();
+getUrl();
