15c721676c11607731e89379257e1c5d2744d810
Rithvik Kasarla <rithvikrk719@gmail.com>
2020-09-19 22:45:49 -0400

    Got active URL in javascript

--- chrome-extension/src/js/popup.js
+++ chrome-extension/src/js/popup.js
@@ -1,4 +1,16 @@
 import "../css/popup.css";
-import hello from "./popup/example";
-
-hello();
+function setup() {
+  let params = {
+    active: true,
+    currentWindow: true,
+  };
+  chrome.tabs.query(params, gotTab);
+  var username = "Chair"; // FROM BACKEND WHEN LOGINING
+  function gotTab(tabs) {
+    console.log("Hello from Rithvik");
+    console.log(tabs);
+    var val = tabs[0].url;
+    Url.textContent = tabs[0].url;
+  }
+}
+setup();
